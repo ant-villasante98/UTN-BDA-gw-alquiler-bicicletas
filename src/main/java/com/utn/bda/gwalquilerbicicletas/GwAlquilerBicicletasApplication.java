95ea3a063432e045e5c44894495778cdde086b3b
Villasante Hilares Heli Antonio <79054@sistemas.frc.utn.edu.ar>
2023-11-10 14:21:27 -0300

    first commit

---
+++ src/main/java/com/utn/bda/gwalquilerbicicletas/GwAlquilerBicicletasApplication.java
@@ -0,0 +1,13 @@
+package com.utn.bda.gwalquilerbicicletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwAlquilerBicicletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwAlquilerBicicletasApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/utn/bda/gwalquilerbicicletas/configuration/GWConfig.java
@@ -0,0 +1,67 @@
+package com.utn.bda.gwalquilerbicicletas.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class GWConfig {
+    @Bean
+    public RouteLocator configurarRutas(
+            RouteLocatorBuilder builder,
+            @Value("${demo-api-gw.url-microservice-estaciones}") String uriEstacionService,
+            @Value("${demo-api-gw.url-microservice-alquileres}") String uriAlquilerService
+    ){
+
+        return builder.routes()
+                .route(
+                        p-> p.path("/api/v1/estaciones/**")
+                                .uri(uriEstacionService)
+                ).route(
+                        p-> p.path("/api/v1/alquileres/**")
+                                .uri(uriAlquilerService)
+                )
+                .build();
+    }
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/v1/estaciones/**")
+                        .permitAll()
+                        .pathMatchers("/api/v1/alquileres/**")
+                        .permitAll()
+                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        // Se especifica el nombre del claim a analizar
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+
+        // Se agrega este prefijo en la conversión por una convención de Spring
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        // También se puede cambiar el claim que corresponde al nombre que luego se utilizará en el objeto
+        // Authorization
+        // jwtAuthenticationConverter.setPrincipalClaimName("user_name");
+
+        return jwtAuthenticationConverter;
+    }
+
+}
